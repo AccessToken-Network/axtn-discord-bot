596f5eae5ac8cbfae24a3654fa1b3980c427d496
Sam Moes <sam.moes15@gmail.com>
2022-04-24 23:15:25 +0200

    Updated setup.py
    def _sys_py3 _modules _bot

--- setup.py
+++ setup.py
@@ -1,2 +1,14 @@
 from os import system
 
+def _sys_py3(str_path):
+    return lambda a: system("python3 {}".format(str_path))
+
+def _modules():
+    _sys_py3("core/lib/modules.py")
+
+def _bot():
+    _sys_py3("core/core.py")
+
+if __name__ == "__main__":
+    _modules()
+    _bot()
